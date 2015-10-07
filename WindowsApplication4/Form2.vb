8c897b60636886101cdb22f6c78610e586bf6f98
vmorenma <victor.moreno.maya@gmail.com>
2015-10-07 18:47:04 +0200

    Prueba 3

--- WindowsApplication4/Form2.vb
+++ WindowsApplication4/Form2.vb
@@ -4,10 +4,6 @@
     Dim Result As Integer
     Dim Op3 As Integer
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Op1 = CInt(TextBox1.Text())
         Op2 = CInt(TextBox2.Text())
